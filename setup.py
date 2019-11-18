c1b896391ea10d4da70e0647b47167ed8be19c3b
Steven Carter <stevenca@cisco.com>
2019-11-18 10:52:17 -0500

    Fixed finding packages

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="viptela",
     version='0.1.7',
     packages=find_packages(),
