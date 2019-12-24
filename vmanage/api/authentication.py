ee80c6e6cde8b0be2c83a17090d2dfcc1fe9ede7
Mike Pfeiffer <michael.pfeiffer@wwt.com>
2019-12-24 11:30:02 -0600

    Adds copyright header, __init__ and login functions

--- vmanage/api/authentication.py
+++ vmanage/api/authentication.py
@@ -23,6 +23,66 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+from __future__ import (
+    absolute_import,
+    division,
+    print_function
+)
+
+import json
+import requests
+import os
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 class Authentication(object):
-    # TODO
-    pass
+    def __init__(self, host=None, user=None, password=None, port=443,
+                validate_certs=False, disable_warnings=False, timeout=10):
+        self.headers = dict()
+        self.cookies = None
+        self.json = None
+        self.method = None
+        self.path = None
+        self.response = None
+        self.status = None
+        self.url = None
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        self.timeout = os.getenv('VMANAGE_TIMEOUT', timeout)
+        self.base_url = 'https://{0}:{1}/dataservice'.format(self.host, self.port)
+        self.session = requests.Session()
+        self.session.verify = validate_certs
+        self.policy_list_cache = {}
+        self.login()
+
+
+    def login(self):
+        try:
+            response = self.session.post(
+                url='{0}/j_security_check'.format(self.base_url),
+                headers={'Content-Type': 'application/x-www-form-urlencoded'},
+                data={'j_username': self.user, 'j_password': self.password},
+                timeout=self.timeout
+            )
+        except requests.exceptions.RequestException as e:
+            raise Exception('Could not connect to {0}: {1}'.format(self.host, e))
+
+        if response.status_code != 200 or response.text.startswith('<html>'):
+            raise Exception('Could not login to device, check user credentials.')
+
+        response = self.session.get(
+            url='https://{0}/dataservice/client/token'.format(self.host),
+            timeout=self.timeout
+        )
+        if response.status_code == 200:
+            self.session.headers['X-XSRF-TOKEN'] = response.content
+        elif response.status_code == 404:
+            # Assume this is pre-19.2
+            pass
+        else:
+            raise Exception('Failed getting X-XSRF-TOKEN: {0}'.format(response.status_code))
+
+        return self.session
