10f23974d916f637e4563fbe7ada8ce879e239a7
Mike Pfeiffer <michael.pfeiffer@wwt.com>
2019-12-24 11:30:40 -0600

    Adds copyright header, changes method session to authentication

--- vmanage/cli/vmanage.py
+++ vmanage/cli/vmanage.py
@@ -1,8 +1,33 @@
+"""Cisco vManage SDK CLI.
+
+MIT License
+
+Copyright (c) 2019 Cisco Systems and/or its affiliates.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import click
 from vmanage.cli.show import show
 from vmanage.cli.export import export
 from vmanage.cli.import_cmd import import_cmd
-from vmanage.api import vmanage_session
+from vmanage.api.authentication import Authentication
 
 class CatchAllExceptions(click.Group):
 
@@ -28,7 +53,7 @@ class Vmanage(object):
 @click.pass_context
 def main(ctx, host, username, password):
 
-    ctx.obj = vmanage_session(host=host, user=username, password=password)
+    ctx.obj = Authentication(host=host, user=username, password=password)
 
 main.add_command(show)
 main.add_command(export)
