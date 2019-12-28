ff50bb0577428ba262875c4a37beab26d8359395
Mike Pfeiffer <michael.pfeiffer@wwt.com>
2019-12-28 10:38:24 -0600

    Refactors Authentication class
    * Uses pycodestyle to validate PEP8 compliance
    * Adds docstrings to class and functions
    * Performs version check for x-xsrf-token GET
    * Places all code in try/except block
    * Removes unused imports
    * Temporarily removes unused variables

--- vmanage/api/authentication.py
+++ vmanage/api/authentication.py
@@ -31,62 +31,97 @@ from __future__ import (
 
 import json
 import requests
-import os
 import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+
 class Authentication(object):
+    """vManage Authentication API
+
+    Responsible for retrieving the JSESSIONID after a username/password
+    has been authenticated.  If the vManage version is >= 19.2.0 then
+    the X-XSRF-TOKEN will be retrieved and added to the header.  An
+    HTTP(S) Request session object will be returned.
+
+    """
+
     def __init__(
         self, host=None, user=None, password=None, port=443,
-        validate_certs=False, disable_warnings=False, timeout=10
+        validate_certs=False, timeout=10
     ):
-        self.headers = dict()
-        self.cookies = None
-        self.json = None
-        self.method = None
-        self.path = None
-        self.response = None
-        self.status = None
-        self.url = None
+        """Initialize Authentication object with session parameters.
+
+        Args:
+            host (str): hostname or IP address of vManage
+            user (str): username for authentication
+            password (str): password for authentication
+            port (int): default HTTPS port 443
+            validate_certs (bool): turn certificate validation
+                on or off.
+            timeout (int): how long Reqeusts will wait for a
+                response from the server, default 10 seconds
+
+        """
+
+        self.host = host
         self.user = user
         self.password = password
-        self.host = host
         self.port = port
-        self.timeout = os.getenv('VMANAGE_TIMEOUT', timeout)
-        self.base_url = 'https://{0}:{1}/dataservice'.format(self.host, self.port)
+        self.timeout = timeout
+        self.base_url = f'https://{self.host}:{self.port}/dataservice/'
         self.session = requests.Session()
         self.session.verify = validate_certs
-        self.policy_list_cache = {}
 
     def login(self):
-        
+        """Executes login tasks against vManage to retrieve token(s).
+
+        Args:
+            None.
+
+        Returns:
+            self.session: a Requests session with JSESSIONID and an
+            X-XSRF-TOKEN for vManage version >= 19.2.0.
+
+        Raises:
+            LoginFailure: If the username/password are incorrect.
+            RequestException: If the host is not accessible.
+
+        """
+
         try:
+            api = 'j_security_check'
+            url = f'{self.base_url}{api}'
             response = self.session.post(
-                url='{0}/j_security_check'.format(self.base_url),
-                headers={'Content-Type': 'application/x-www-form-urlencoded'},
+                url=url,
                 data={'j_username': self.user, 'j_password': self.password},
                 timeout=self.timeout
             )
 
+            if (
+                response.status_code != 200 or
+                response.text.startswith('<html>')
+            ):
+                raise Exception('Login failed, check user credentials.')
+
+            api = 'system/device/controllers?model=vmanage&&&&'
+            url = f'{self.base_url}{api}'
+            response = self.session.get(
+                url=url,
+                timeout=self.timeout
+            )
+            version = json.loads(response.text)['data'][0]['version']
+
+            if (version >= '19.2.0'):
+                api = 'client/token'
+                url = f'{self.base_url}{api}'
+                response = self.session.get(
+                    url=url,
+                    timeout=self.timeout
+                )
+                self.session.headers['X-XSRF-TOKEN'] = response.content
+
         except requests.exceptions.RequestException as e:
-            raise Exception('Could not connect to {0}: {1}'.format(self.host, e))
+            raise Exception(f'Could not connect to {self.host}: {e}')
 
-        if response.status_code != 200 or response.text.startswith('<html>'):
-            raise Exception('Could not login to device, check user credentials.')
-
-        response = self.session.get(
-            url='https://{0}/dataservice/client/token'.format(self.host),
-            timeout=self.timeout
-        )
-        if response.status_code == 200:
-            self.session.headers['X-XSRF-TOKEN'] = response.content
-            
-        elif response.status_code == 404:
-            # Assume this is pre-19.2
-            pass
-        else:
-            raise Exception('Failed getting X-XSRF-TOKEN: {0}'.format(response.status_code))
-
-        
         return(self.session)
