e4577da4f28b14f61e01c088a4cf7e0dde9bf8da
Mike Pfeiffer <michael.pfeiffer@wwt.com>
2019-12-29 14:20:25 -0600

    Adds documentation and code update
    * Adds docstrings for class and functions
    * Adds functions for control connections
    * Adds function for error handling
    * Adds function for device status

--- vmanage/api/monitor_network.py
+++ vmanage/api/monitor_network.py
@@ -23,6 +23,99 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-class MonitorNetworks(object):
-    # TODO
-    pass
+import json
+import requests
+from vmanage.api.http_methods import HttpMethods
+
+
+class MonitorNetwork(object):
+    """vManage Monitor Networks API
+
+    Responsible for GET methods against vManage Real Time Monitoring
+    for network devices.
+
+    """
+
+    def __init__(self, session, host, port=443):
+        """Initialize Monitor Networks object with session parameters.
+
+        Args:
+            session (obj): Requests Session object
+            host (str): hostname or IP address of vManage
+            port (int): default HTTPS 443
+
+        """
+
+        self.session = session
+        self.host = host
+        self.port = port
+        self.base_url = f'https://{self.host}:{self.port}/dataservice/'
+
+    def error_handling(self, response):
+        """Error Handling for Empty Data.
+
+        Args:
+            response (obj): Requests response object
+
+        Returns:
+            result (dict): All data associated with a response.
+
+        Raises:
+            Exception: Provides error message and details of issue.
+        """
+
+        if 'data' in response['json']:
+            result = response['json']['data']
+        else:
+            error = response['error']
+            result = response['details']
+            raise Exception(f'{error}: {result}')
+        return(result)
+
+    def get_control_connections(self, system_ip):
+        """Provides current control connections for device.
+
+        Args:
+            system_ip (str): Device System IP
+
+        Returns:
+            result (dict): All data associated with a response.
+        """
+
+        api = "device/control/connections?deviceId=" + system_ip
+        url = self.base_url + api
+        response = HttpMethods(self.session, url).request('GET')
+        result = self.error_handling(response)
+        return(result)
+
+    def get_control_connections_history(self, system_ip):
+        """Provides control connections history for device.
+
+        Args:
+            system_ip (str): Device System IP
+
+        Returns:
+            result (dict): All data associated with a response.
+        """
+
+        api = "device/control/connectionshistory?deviceId=" + system_ip
+        url = self.base_url + api
+        response = HttpMethods(self.session, url).request('GET')
+        result = self.error_handling(response)
+        return(result)
+
+    def get_device_status(self, system_ip):
+        """Provides status for device.
+
+        Args:
+            system_ip (str): Device System IP
+
+        Returns:
+            result (dict): All data associated with a response.
+        """
+
+        api = "device?system-ip=" + system_ip
+        url = self.base_url + api
+        response = HttpMethods(self.session, url).request('GET')
+        result = self.error_handling(response)
+        return(result)
