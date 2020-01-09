8d590066b5b0ab6d6f65af0bc171f27972fb6183
ttafsir <ttafsir@gmail.com>
2020-01-09 17:48:24 -0500

    - Add python requirement to setup - Add long description and long description content type to setup

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ setup(
     version='0.1.7',
     packages=find_packages(),
     description="Cisco DevNet Viptela vManage CLI/SDK",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.6',
     install_requires=[
         'Click',
         'requests',
