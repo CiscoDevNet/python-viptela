6a26966f969c6a0381f6c507d32b395fbc32d7e6
Steven Carter <stevenca@cisco.com>
2020-04-11 10:00:26 -0400

    Added CLI tests

---
+++ tests/cli.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+vmanage show device status
+
+vmanage show device config
+
+vmanage show templates
+
+vmanage show policies list
+
+vmanage show policies definition
+
+vmanage show policies local
+
+vmanage show policies central
+
+vmanage import templates --file tests/vmanage-templates.yml
+
+vmanage import policies --file tests/vmanage-policies.yml
+
+vmanage export templates --file /tmp/vmanage-templates-cli.yml
+
+vmanage export policies --file /tmp/vmanage-policies-cli.yml
