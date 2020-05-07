7dd00db8091b39628f95efa35b6b8d99ecc8b28e
Josh Lothian <jlothian@cisco.com>
2020-05-07 11:09:50 -0500

    Transition to autoapi for less maintenance and complication (#45)
    Convert from autodoc to autoapi
    
    This lets us build more cleanly

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,6 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../vmanage'))
-sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -32,7 +30,6 @@ release = '0.2.3'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-  'sphinx.ext.autodoc',
   'sphinx.ext.napoleon',
   "sphinx_rtd_theme",
   'sphinx.ext.todo',
@@ -40,6 +37,9 @@ extensions = [
   'm2r'
 ]
 
+extensions.append('autoapi.extension')
+autoapi_type = 'python'
+autoapi_dirs = ['../../vmanage']
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
