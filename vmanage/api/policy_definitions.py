f532704a1c0dbb7379f6be68bb4d14570f1574fd
Seth Beauchamp <gcm707@gmail.com>
2020-07-11 12:41:23 -0400

    Correcting local commit mix up that pushed wrong code in previous commit

--- vmanage/api/policy_definitions.py
+++ vmanage/api/policy_definitions.py
@@ -53,7 +53,7 @@ class PolicyDefinitions(object):
         if definition_type == "advancedMalwareProtection":
             url = f"{self.base_url}template/policy/definition/{definition_type}/{definition_id}"
         else:
-            url = f"{self.base_url}template/policy/definition/{definition_type}.lower()/{definition_id}"
+            url = f"{self.base_url}template/policy/definition/{definition_type.lower()}/{definition_id}"
 
         HttpMethods(self.session, url).request('DELETE')
 
@@ -72,7 +72,7 @@ class PolicyDefinitions(object):
         if policy_definition == "advancedMalwareProtection":
             url = f"{self.base_url}template/policy/definition/{policy_definition}"
         else:
-            url = f"{self.base_url}template/policy/definition/{policy_definition}.lower()"
+            url = f"{self.base_url}template/policy/definition/{policy_definition.lower()}"
 
         HttpMethods(self.session, url).request('POST', payload=json.dumps(policy_definition))
 
@@ -91,7 +91,7 @@ class PolicyDefinitions(object):
         if policy_definition == "advancedMalwareProtection":
             url = f"{self.base_url}template/policy/definition/{policy_definition}/{policy_definition_id}"
         else:
-            url = f"{self.base_url}template/policy/definition/{policy_definition}.lower()/{policy_definition_id}"
+            url = f"{self.base_url}template/policy/definition/{policy_definition.lower()}/{policy_definition_id}"
 
         HttpMethods(self.session, url).request('PUT', payload=json.dumps(policy_definition))
 
@@ -110,7 +110,7 @@ class PolicyDefinitions(object):
         if definition_type == "advancedMalwareProtection":
             url = f"{self.base_url}template/policy/definition/{definition_type}/{definition_id}"
         else:
-            url = f"{self.base_url}template/policy/definition/{definition_type}.lower()/{definition_id}"
+            url = f"{self.base_url}template/policy/definition/{definition_type.lower()}/{definition_id}"
 
         response = HttpMethods(self.session, url).request('GET')
 
