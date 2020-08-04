1fa979aac8ed46c25cd2deee6c5e4df20fd7e5d1
David Mulveney <dmulvene@cisco.com>
2020-08-04 17:32:23 -0400

    fix pylint errors

--- vmanage/api/device_templates.py
+++ vmanage/api/device_templates.py
@@ -436,4 +436,3 @@ class DeviceTemplates(object):
         url = f"{self.base_url}template/config/running/{uuid}"
         response = HttpMethods(self.session, url).request('GET')
         return ParseMethods.parse_config(response)
-
