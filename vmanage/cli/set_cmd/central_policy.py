6de355c16d8cf2ae61d38c1c6ac000549e16043b
jasonking3 <jk1ng@yahoo.com>
2020-08-13 10:12:22 -0700

    Removed single dash options and added trailing underscore for type

--- vmanage/cli/set_cmd/central_policy.py
+++ vmanage/cli/set_cmd/central_policy.py
@@ -3,18 +3,17 @@ from vmanage.api.policy_updates import PolicyUpdates
 
 
 @click.command('central-policy')
-@click.option('--type', '-t', help="Policy Type (supported option is approute)", required=True)
+@click.option('--type', '-t', 'type_', help="Policy Type (supported option is approute)", required=True)
 @click.option('--name', '-n', help="Policy name", required=True)
-@click.option('--pref-color', '-prefcolor', help="Preferred color", required=True)
-@click.option('--seq-name', '-seqname', help="Approute policy sequence name", required=False, default=None)
+@click.option('--pref-color', help="Preferred color", required=True)
+@click.option('--seq-name', help="Approute policy sequence name", required=False, default=None)
 @click.pass_obj
-# pylint: disable=redefined-builtin
-def central_policy(ctx, type, name, pref_color, seq_name):
+def central_policy(ctx, type_, name, pref_color, seq_name):
     """
     Set Policy sequence statements
     """
 
     vmanage_policy_updates = PolicyUpdates(ctx.auth, ctx.host)
-    policy_id = vmanage_policy_updates.get_policy_id(type, name)
-    policy_def = vmanage_policy_updates.get_policy_definition(type, policy_id)
-    vmanage_policy_updates.update_policy_definition(type, name, policy_id, policy_def, pref_color, seq_name)
+    policy_id = vmanage_policy_updates.get_policy_id(type_, name)
+    policy_def = vmanage_policy_updates.get_policy_definition(type_, policy_id)
+    vmanage_policy_updates.update_policy_definition(type_, name, policy_id, policy_def, pref_color, seq_name)
