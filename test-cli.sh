ca1b25f23a8709ab5003bb85d130563ecb57140a
jasonking3 <jk1ng@yahoo.com>
2020-12-10 13:02:14 -0800

    Improved string compare

--- test-cli.sh
+++ test-cli.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 input="./tests/commands.csv"
+
+function version_gt() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"; }
+
 if [[ $1 == "" ]]; then
     version="19.2.1"
 else
     version=$1
 fi
+
 while IFS=',' read -r cmdver line
 do
-    if [[ $version > $cmdver || $version == $cmdver ]]; then
+    if version_gt $version $cmdver || [[ $version == $cmdver ]]; then
         printf "Running: $line..."
         $line > /dev/null
         if [ $? -eq 0 ]; then
